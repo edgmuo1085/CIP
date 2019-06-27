27d5ec7a0d3a9f8e53c5fc68a2024901ecef2f0c
edgmuo1085 <edgmuo1085@gmail.com>
2019-06-26 20:27:55 -0500

    Agregacion de otro boton y que al hacer clic en ellos sume y reste

--- window.py
+++ window.py
@@ -4,14 +4,31 @@
 #GUI library
 import os # Librería para limpiar pantalla
 from random import randint, uniform, random
+#Gestionar Widgets: Buttons, labels, inputs, etc
 from tkinter import ttk
 import tkinter as tk
 
+#Definir variables
+num = 0
+def saludo():
+	print("Has hecho clic")
+
+def contar():
+	global num
+	num += 1
+	label.config(text=str(num))
+	print(num)
+
+def restar():
+	global num
+	num -= 1
+	label.config(text=str(num))
+	print(num)
 
 #os.system("clear") # Borrar pantalla
 mi_ventana = tk.Tk()
 
-#icono
+#icono de ventana
 #mi_ventana.iconbitmap('iconos/favicon.ico')
 #Titulo
 mi_ventana.title("MI PRIMER VENTANA")
@@ -21,6 +38,15 @@ mi_ventana.geometry("600x600+500+50")
 mi_ventana.resizable(width=False, height=True)
 # background de la ventana
 mi_ventana.config(bg="#CFACAC")
-boton = tk.Button(mi_ventana, text="Ejecutar").place(x=60, y=40, width=100, height=100)
+
+#lambda: se ejecuta la funcion antes de dar clic en el boton
+boton1 = tk.Button(mi_ventana, text="Sumar 1", command=contar).place(x=60, y=40, width=100, height=100)
+boton2 = tk.Button(mi_ventana, text="Restar 1", command=restar).place(x=170, y=40, width=100, height=100)
+
+label = tk.Label(mi_ventana, fg="dark green")
+label.pack()
+
+
+
 #Ejecutar ventana
 mi_ventana.mainloop()
